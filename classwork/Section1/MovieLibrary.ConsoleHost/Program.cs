c34ea91c5f9443e7fea8d70707045d8b84c19b41
Faith Staskus <faith.staskus@my.tccd.edu>
2022-08-29 21:05:29 -0500

    Starting movie project

---
+++ classwork/Section1/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,17 @@
+﻿// Movie Definintion
+// Faith Staskus
+// Start Date 8/29/22
+
+string title = "";
+string description = "";
+int runLength = 0;          // in minutes
+int releaseYear = 1900;
+string rating = "";
+bool isClassic = false;
+
+Console.WriteLine("Enter a title: ");
+Console.WriteLine("Enter an optional description: ");
+Console.WriteLine("Enter a run length (in minutes): ");
+Console.WriteLine("Enter the release year: ");
+Console.WriteLine("Enter a MPAA rating: ");
+Console.WriteLine("Is this a classic: ");
