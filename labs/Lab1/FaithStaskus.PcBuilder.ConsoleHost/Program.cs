4512627804447cc557b1b8a59c899d3560c55071
Faith Staskus <faith.staskus@my.tccd.edu>
2022-09-12 20:16:23 -0500

    Started Story 3

--- labs/Lab1/FaithStaskus.PcBuilder.ConsoleHost/Program.cs
+++ labs/Lab1/FaithStaskus.PcBuilder.ConsoleHost/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lab 1
+// Faith Staskus
+// ITSE 1430
+// 9/21/2022
+
+DisplayInformation();
+DisplayMenu();
+
+// functions
+void DisplayInformation ()
+{
+    Console.WriteLine("Faith Staskus");
+    Console.WriteLine("ITSE 1430");
+    Console.WriteLine("Fall 2022");
+
+}
+
+void DisplayMenu ()
+{
+    do
+    {
+        Console.WriteLine("1. Quit")
+
+
+
+    }while (true);
+ }
